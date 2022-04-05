e989bda32346d2c9ec6b1b4e2ae336c5b762fb9b
GladysRosalesM <98922401+GladysRosalesM@users.noreply.github.com>
2022-04-04 19:32:07 -0600

    Add files via upload

---
+++ Ejercicio - 45 - Punto-de-venta.h/Factura.h
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void productos (int opcion);
+void imprimirFactura();
+void menu();
+void agregarProducto(string descripcion, int cantidad, double precio);
+void imprimirFactura();
+void productos(int opcion);
+void agregarProducto(string descripcion, int cantidad, double precio);
+void imprimirFactura();
+
---
+++ Ejercicio - 45 - Punto-de-venta.h/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Factura.h"
+
+using namespace std;
+
+
+int main(int argc, char** argv) 
+{
+	menu();
+	
+}
